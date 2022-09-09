ff0b22619eacb23441188705975836f7b3fcff15
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-09 08:10:14 +0530

    Adding program for Creating Address Book Table with first and last names, address, city, state, zip, phone number and email as its attributes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AddressBookUsingLINQ;
+
 Console.WriteLine("Wellcome to the Address Book Problem Using LINQ");
+
+AddressBook addressbook1 = new AddressBook();
+addressbook1.AddToDataTable();
+addressbook1.DisplayLastNameFromTable();
+addressbook1.DisplayAddressFromTable();
+addressbook1.DisplayCityFromTable();
+addressbook1.DisplayStateFromTable();
+addressbook1.DisplayZipCodeFromTable();
+addressbook1.DisplayPhoneNumberFromTable();
+addressbook1.DisplayEmailFromTable();
+
+

5ac0d09c2eec3b68542aa2ab6234f418c546c199
milanbiranwar18 <109934967+milanbiranwar18@users.noreply.github.com>
2022-09-09 16:46:27 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,12 +5,5 @@ Console.WriteLine("Wellcome to the Address Book Problem Using LINQ");
 
 AddressBook addressbook1 = new AddressBook();
 addressbook1.AddToDataTable();
-addressbook1.DisplayLastNameFromTable();
-addressbook1.DisplayAddressFromTable();
-addressbook1.DisplayCityFromTable();
-addressbook1.DisplayStateFromTable();
-addressbook1.DisplayZipCodeFromTable();
-addressbook1.DisplayPhoneNumberFromTable();
-addressbook1.DisplayEmailFromTable();
 
 
